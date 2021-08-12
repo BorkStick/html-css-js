8ce03f6a51421bbbcf52ed671124dd13aecc920a
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-08-11 20:14:06 -0500

    workig on pokemon card

--- content-placeholder/main.js
+++ content-placeholder/main.js
@@ -11,13 +11,13 @@ const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
 setTimeout(getData, 2500)
 
 function getData() {
-    // header.innerHTML = '<img src="http://tylor.tech/wp-content/uploads/2020/06/Tylor-Head-2020-Square-web.png"/>'
+    //header.innerHTML = '<img src="http://tylor.tech/wp-content/uploads/2020/06/Tylor-Head-2020-Square-web.png"/>'
     title.innerHTML = "Lorem ipsum"
     excerpt.innerHTML = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, quas!'
     profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/women/42.jpg"/>'
     name.innerHTML = "Jane Doe"
     date.innerHTML = '&nbsp;'
-
+    
     animated_bgs.forEach(bg => bg.classList.remove('animated-bg'))
     animated_bg_texts.forEach(bg => bg.classList.remove('animated-bg-text'))
 }
