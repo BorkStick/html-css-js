9ced44077ea64e89173e3f1a7374fea1d4e42733
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-09-15 22:41:17 -0500

    added readme and screenshot to content-placeholder

--- content-placeholder/main.js
+++ content-placeholder/main.js
@@ -1,37 +1,25 @@
-const header = document.getElementById('header')
-const title = document.getElementById('title')
-const excerpt = document.getElementById('excerpt')
-const profile_img = document.getElementById('profile-img')
-const name = document.getElementById('name')
-const date = document.getElementById('date')
+const header = document.getElementById("header");
+const title = document.getElementById("title");
+const excerpt = document.getElementById("excerpt");
+const profile_img = document.getElementById("profile-img");
+const name = document.getElementById("name");
+const date = document.getElementById("date");
 
-const animated_bgs = document.querySelectorAll('.animated-bg')
-const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+const animated_bgs = document.querySelectorAll(".animated-bg");
+const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
 
-setTimeout(getData, 2500)
+setTimeout(getData, 2500);
 
 function getData() {
-    //header.innerHTML = '<img src="http://tylor.tech/wp-content/uploads/2020/06/Tylor-Head-2020-Square-web.png"/>'
-    title.innerHTML = "Lorem ipsum"
-    excerpt.innerHTML = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, quas!'
-    profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/women/42.jpg"/>'
-    name.innerHTML = "Jane Doe"
-    date.innerHTML = '&nbsp;'
-    
-    animated_bgs.forEach(bg => bg.classList.remove('animated-bg'))
-    animated_bg_texts.forEach(bg => bg.classList.remove('animated-bg-text'))
+  //header.innerHTML = '<img src="http://tylor.tech/wp-content/uploads/2020/06/Tylor-Head-2020-Square-web.png"/>'
+  title.innerHTML = "Lorem ipsum";
+  excerpt.innerHTML =
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, quas!";
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/women/42.jpg"/>';
+  name.innerHTML = "Jane Doe";
+  date.innerHTML = "&nbsp;";
+
+  animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"));
+  animated_bg_texts.forEach((bg) => bg.classList.remove("animated-bg-text"));
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
